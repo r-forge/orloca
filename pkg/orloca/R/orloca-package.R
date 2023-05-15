916648dfc8217270e3cd93da89516bffa4a390f5
mmarquez (Dr ) <manuel.munoz.marquez@gmail.com>
2023-05-15 21:48:36 +0000

    orloca 5.2 (565)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/orloca@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/orloca/R/orloca-package.R
+++ pkg/orloca/R/orloca-package.R
@@ -2,9 +2,9 @@
 #' 
 #' Objects and methods to handle and solve the min-sum location problem, also known as Fermat-Weber problem.
 #'
-#' The min-sum location problem search for a point such that the weighted sum of the distances to the demand points are minimized. See "The Fermat-Weber location problem revisited" by Brimberg, Mathematical Programming, 1, pg. 71-76, 1995, DOI:10.1007/BF01592245.
+#' The min-sum location problem search for a point such that the weighted sum of the distances to the demand points are minimized. See "The Fermat-Weber location problem revisited" by Brimberg, Mathematical Programming, 1, pg. 71-76, 1995, \doi{10.1007/BF01592245}.
 #'
-#' General global optimization algorithms are used to solve the problem, along with the adhoc Weiszfeld method, see "Sur le point pour lequel la Somme des distances de n points donnes est minimum", by E. Weiszfeld, Tohoku Mathematical Journal, First Series, 43, pg. 355-386, 1937 or "On the point for which the sum of the distances to n given points is minimum", by E. Weiszfeld and F. Plastria, Annals of Operations Research, 167, pg. 7-41, 2009, DOI:10.1007/s10479-008-0352-z.
+#' General global optimization algorithms are used to solve the problem, along with the adhoc Weiszfeld method, see "Sur le point pour lequel la Somme des distances de n points donnes est minimum", by E. Weiszfeld, Tohoku Mathematical Journal, First Series, 43, pg. 355-386, 1937 or "On the point for which the sum of the distances to n given points is minimum", by E. Weiszfeld and F. Plastria, Annals of Operations Research, 167, pg. 7-41, 2009, \doi{10.1007/s10479-008-0352-z}.
 #'
 #' @aliases orloca-package
 #' @docType package
@@ -16,9 +16,9 @@
 #' 
 #' Type:      Package
 #' 
-#' Version:   5.1
+#' Version:   5.2
 #' 
-#' Date:      2023-05-15
+#' Date:      2023-05-16
 #' 
 #' License:   GPL (>= 3)
 #' }
